bfad1c5d7479273af1126b793db611ef87979e6e
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-06-05 11:34:16 +0700

    เพิ่ม Lecture 4 เรื่อง Common JS

---
+++ lecture4/my-module01.js
@@ -0,0 +1,14 @@
+let dataX = 'Hello...'
+
+const SAU = 'Southeast Asia University'
+
+function showWow() {
+    console.log('Wow wow wow')
+}
+
+//เป็นสร้าง Module แบบ CommonJS
+module.exports = {
+    dataX,
+    SAU,
+    showWow
+}
---
+++ lecture4/my-module02.js
@@ -0,0 +1,9 @@
+let dataY = 'Hey...'
+
+function sumNumber(num1, num2){
+    return num1 + num2
+}
+
+//เป็นสร้าง Module แบบ CommonJS
+exports.dataY = dataY
+exports.sumNumber = sumNumber
